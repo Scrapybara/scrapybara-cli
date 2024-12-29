7590ed3eabacf6d14d52510b44504fea043bace4
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-12-30 00:02:28 +0100

    basic

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import typer
 from scrapybara import Scrapybara
 from dotenv import load_dotenv
+from rich.console import Console
 import os
 
 load_dotenv()
 
+console = Console()
 scrapybara = Scrapybara(api_key=os.getenv("SCRAPYBARA_API_KEY"))
 
 def main(instance_type: str = "small"):
@@ -17,7 +19,15 @@ def main(instance_type: str = "small"):
     if instance_type not in ["small", "medium", "large"]:
         raise ValueError('instance_type must be one of: "small", "medium", "large"')
     
-    print(instance_type)
+    try:
+        with console.status("[bold green]Starting instance...", spinner="dots") as status:
+            instance = scrapybara.start(instance_type=instance_type)
+            status.update("[bold green]Instance started!")
+
+    finally:
+        with console.status("[bold red]Stopping instance...", spinner="dots") as status:
+            instance.stop()
+            status.update("[bold red]Instance stopped!")
     
 
 if __name__ == "__main__":
