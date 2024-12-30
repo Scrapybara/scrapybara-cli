52fbe8034bef5f48681700e9542071a6db81a7f9
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-12-30 01:02:01 +0100

    renamed

---
+++ scrapybara_cli/agent.py
@@ -0,0 +1,4 @@
+from scrapybara_cli.prompt import SYSTEM_PROMPT
+from scrapybara_cli.helpers import ToolCollection, make_tool_result
+from scrapybara.client import Instance
+from anthropic import Anthropic
